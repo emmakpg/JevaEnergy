2975f847447888c004008da9504f23fb284e8576
emmakpg <emmakpg@yahoo.com>
2024-09-13 20:50:27 +0000

    update

--- src/components/PCard.jsx
+++ src/components/PCard.jsx
@@ -4,11 +4,12 @@ import Image from "next/image"
 const PCard = ({imageUrl,label,value}) => {
   return (
     <div className="bg-white shadow-md rounded-md grid lg:grid-cols-[2fr,4fr] grid-cols-1">
-         <div className=" text-primary lg:pr-4"> <Image
+         <div className=" text-primary lg:pr-4 "> <Image
           src={imageUrl}
           alt="jeva_core_values"
           width={500}
           height={500}
+          className="rounded-l-lg"
           />
           </div>
           <div className="lg:mt-20 py-4 pb-8 px-4"> 
