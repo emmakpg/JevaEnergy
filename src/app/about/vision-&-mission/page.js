8427168658fbcb9ff7c335b3a7edc09440019a01
emmakpg <emmakpg@yahoo.com>
2024-09-19 09:03:31 +0000

    update

--- src/app/about/vision-&-mission/page.js
+++ src/app/about/vision-&-mission/page.js
@@ -14,20 +14,20 @@ const VisionMission = () => {
 <section className=" bg-[#f5f5f5]">
 
   <div className="max-container py-4">
-  <div className="flex  mt-24">
+  <div className="flex  lg:mt-24 mt-10">
         <Divider divideColor="bg-primary"/>
         </div>
-          <h2 className="text-primary text-[30px] font-medium m-6 ml-4">{AboutContent.vision_title}</h2>
+          <h2 className="text-primary text-[30px] leading-8 font-medium m-6 ml-4">{AboutContent.vision_title}</h2>
 
-          <p className="text-lg font-thin leading-normal ml-4 lg:mr-52">{AboutContent.vision_content}</p>
+          <p className="lg:text-lg font-thin leading-normal ml-4 lg:mr-52">{AboutContent.vision_content}</p>
   </div>
   <div className="max-container lg:pb-20">
-  <div className="flex  mt-24">
+  <div className="flex  lg:mt-24 mt-10">
         <Divider divideColor="bg-primary"/>
         </div>
-          <h2 className="text-primary text-[30px] font-medium m-6 ml-4">{AboutContent.mission_title}</h2>
+          <h2 className="text-primary text-[30px] leading-8 font-medium m-6 ml-4">{AboutContent.mission_title}</h2>
 
-          <p className="text-lg font-thin leading-normal ml-4 lg:mr-52">{AboutContent.mission_content}</p>
+          <p className="lg:text-lg font-thin leading-normal ml-4 lg:mr-52 pb-4">{AboutContent.mission_content}</p>
   </div>
   </section>
 </div>
